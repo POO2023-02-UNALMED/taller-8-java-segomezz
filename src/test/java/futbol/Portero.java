b121e96d7fa5ab5f2bd24559491df3fe073d4637
segomezz <141938389+segomezz@users.noreply.github.com>
2023-10-14 11:55:46 -0500

    Update Portero.java

--- src/test/java/futbol/Portero.java
+++ src/test/java/futbol/Portero.java
@@ -3,7 +3,7 @@ package futbol;
 public class Portero extends Futbolista implements Comparable<Futbolista> {
 	public short golesRecibidos;
 	public byte dorsal;
-	public Portero(String nombre,int edad,String posicion,short golesRecibidos,byte dorsal) {
+	public Portero(String nombre,int edad,short golesRecibidos,byte dorsal) {
 		super(nombre,edad,"Portero");
 		this.golesRecibidos=golesRecibidos;
 		this.dorsal=dorsal;
