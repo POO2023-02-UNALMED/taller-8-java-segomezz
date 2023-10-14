953c2eb20d47f6a72c77b40479a2e80f56e9290c
segomezz <141938389+segomezz@users.noreply.github.com>
2023-10-14 11:45:56 -0500

    segundo

--- src/test/java/futbol/Jugador.java
+++ src/test/java/futbol/Jugador.java
@@ -1,5 +1,36 @@
 package futbol;
 
-public class Jugador {
+public class Jugador extends Futbolista implements Comparable<Futbolista>{
+	public short golesMarcados;
+	public byte dorsal;
+	public Jugador() {
+		super();
+		golesMarcados=289;
+		dorsal=7;
+	}
+	public Jugador(String nombre,int edad,String posicion,short golesMarcados,byte dorsal) {
+		super(nombre,edad,posicion);
+		this.golesMarcados=golesMarcados;
+		this.dorsal=dorsal;
+	}
+	@Override
+	public int compareTo(Futbolista otroFutbolista) {
+		if (getEdad() != otroFutbolista.getEdad()) {
+            return Math.abs(Integer.compare(getEdad(), otroFutbolista.getEdad()));
+        }
+		return 0;
+	}
+	@Override
+	public String toString() {
+		return("El futbolista" + super.getNombre() + "tiene" + super.getEdad() + "y juega de" + super.getPosicion() + "con el dorsal" + this.dorsal +". Ha marcado" +this.golesMarcados);
+	}
+	@Override
+	public boolean jugarConLasManos(Futbolista jugador) {
+		if (jugador instanceof Portero) {
+			return true;
+			}
+			return false;
+			
 
+}
 }
--- src/test/java/futbol/Portero.java
+++ src/test/java/futbol/Portero.java
@@ -1,5 +1,35 @@
 package futbol;
 
-public class Portero {
-
+public class Portero extends Futbolista implements Comparable<Futbolista> {
+	public short golesRecibidos;
+	public byte dorsal;
+	public Portero(String nombre,int edad,String posicion,short golesRecibidos,byte dorsal) {
+		super(nombre,edad,"Portero");
+		this.golesRecibidos=golesRecibidos;
+		this.dorsal=dorsal;
+		
+	}
+	@Override
+	public String toString() {
+		return("El futbolista" + super.getNombre() + "tiene" + super.getEdad() + "y juega de" + super.getPosicion() + "con el dorsal" + this.dorsal +". Le han marcado" +this.golesRecibidos);
+	}
+	@Override
+	public boolean jugarConLasManos(Futbolista jugador) {
+		if (jugador instanceof Portero) {
+			return true;
+			}
+			return false;
+}
+	//@Override
+	public int compareTo (Portero otroPortero) {
+		if (golesRecibidos != otroPortero.golesRecibidos) {
+            return Math.abs(Integer.compare(golesRecibidos, otroPortero.golesRecibidos));
+        }
+		return 0;
+}
+	@Override
+	public int compareTo(Futbolista o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
